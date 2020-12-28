7eccad18bab295a0d0d77438519677d1470e85dd
pratiktomar5999 <pratiktomar894@gmail.com>
2020-12-28 16:28:15 +0530

    1st commit

--- app/src/main/java/com/pratik/multimediaapp/MainActivity.java
+++ app/src/main/java/com/pratik/multimediaapp/MainActivity.java
@@ -50,10 +50,9 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, tappedImage.getTag().toString() + "is Tapped", Toast.LENGTH_SHORT).show();
                 tappedImage.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
                 for (int[] winnerColumns : umpire) {
-                    no = no + 1;
                     if (playerChoices[winnerColumns[0]] == playerChoices[winnerColumns[1]]
                             && playerChoices[winnerColumns[1]] == playerChoices[winnerColumns[2]]
-                            && playerChoices[winnerColumns[0]] != Player.NO && no <= 8) {
+                            && playerChoices[winnerColumns[0]] != Player.NO) {
                         String winner = new String();
                         if (currentPlayer == Player.ONE) {
                             winner = "Player Two";
@@ -78,28 +77,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 });
                         dialog.show();
-
-                    }else if (no >= 9){
-
-                        AlertDialog.Builder dialog = new AlertDialog.Builder(this)
-                                .setTitle("Match Draw")
-                                .setCancelable(false)
-                                .setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-
-                                }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        finish();
-                                    }
-                                });
-
-                }
+                    }
             }
         }
     }
